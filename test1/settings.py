2689393030acb797e75bec23963f4f36623fc860
3065923 <burnsjake5253@gmail.com>
2022-05-17 16:33:10 -0400

    Added gitignore and config to hide secret key

--- test1/settings.py
+++ test1/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import config
 import os
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)xbk1_ocpfmnho&0w(jwmaoj2is-^2g-al*32-s_k3#s+4r!c'
+SECRET_KEY = config.my_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
